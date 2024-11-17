3d6b64d52ac1f768825e3131ce5a1d98ffe57413
QuanWenG <154224091+QuanWenG@users.noreply.github.com>
2024-11-17 20:08:05 +0800

    奶龙制糖基础代码

---
+++ src/main/java/com/quanweng/NailongToSugarClient.java
@@ -0,0 +1,10 @@
+package com.quanweng;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NailongToSugarClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/quanweng/NailongToSugarDataGenerator.java
@@ -0,0 +1,11 @@
+package com.quanweng;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NailongToSugarDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
